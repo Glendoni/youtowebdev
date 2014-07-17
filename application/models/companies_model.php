e24d320f9c0f58c1707844a8e82e49b1df61e9fc
Antonio <tottimix@gmail.com>
2014-07-17 10:25:00 +0100

    add composer.json for PHP app detection

--- application/models/companies_model.php
+++ application/models/companies_model.php
@@ -3,8 +3,21 @@ class Companies_model extends CI_Model {
 	
 	function get_all(){
 		$this->db->order_by('id','ASC');
-		$query = $this->db->get('newpublic.companies')->limit('10');
+		$query = $this->db->get('newpublic.companies', 10);
 		return $query->result();
 	}
+
+	function special_query(){
+		$query = $this->db->query('YOUR QUERY HERE');
+		return $query->result();
+	}
+
+	function insert_entry(){
+        $this->title   = $_POST['title']; // please read the below note
+        $this->content = $_POST['content'];
+        $this->date    = time();
+
+        $this->db->insert('entries', $this);
+    }
 }
 ?>
