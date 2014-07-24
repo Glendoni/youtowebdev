14168b7ea5ab4906c91076b7c74623f1c0647af0
Antonio <tottimix@gmail.com>
2014-07-24 15:48:47 +0100

    pushed database setting/ testing env variables

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -67,7 +67,7 @@
 <body>
 
 <div id="container">
-	<h1>Welcome to CodeIgniter! <?php $v = ENVIRONMENT; print($v); ?> </h1>
+	<h1>Welcome to CodeIgniter! <?php $v = getenv('ENVIRONMENT'); print($v); ?> </h1>
 
 	<div id="body">
 		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
