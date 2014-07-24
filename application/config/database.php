5afc518e64adaa60132b1558284e8f4cbb11ebe0
Antonio <tottimix@gmail.com>
2014-07-24 14:59:05 +0100

    pushed database setting/ testing env variables

--- application/config/database.php
+++ application/config/database.php
@@ -48,14 +48,14 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'postgres';
-$db['default']['password'] = 'root';
-$db['default']['database'] = 'baselist';
+$db['default']['hostname'] = 'ec2-54-83-43-49.compute-1.amazonaws.com';
+$db['default']['username'] = 'pafpgwduawjhjg';
+$db['default']['password'] = 'CgceaC6SiUqGVL1zzgTt6egPak';
+$db['default']['database'] = 'dcnitnkm31ke9f';
 $db['default']['dbdriver'] = 'postgre';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
-$db['default']['db_debug'] = TRUE;
+$db['default']['db_debug'] = FALSE;
 $db['default']['cache_on'] = FALSE;
 $db['default']['cachedir'] = '';
 $db['default']['char_set'] = 'utf8';
@@ -63,6 +63,7 @@ $db['default']['dbcollat'] = 'utf8_general_ci';
 $db['default']['swap_pre'] = '';
 $db['default']['autoinit'] = TRUE;
 $db['default']['stricton'] = FALSE;
+$db['default']['port'] = 5432;
 
 
 
