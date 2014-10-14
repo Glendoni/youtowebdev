5e97123295f4d41384ba66b3219090d0b89678c3
Antonio <tottimix@gmail.com>
2014-10-14 11:34:00 +0100

    merge

--- application/views/companies/list.php
+++ application/views/companies/list.php
@@ -21,11 +21,6 @@
 					<a href="<?php echo site_url();?>companies/company?id=<?php echo $company['id'];?>" target="_blank">
 						<?php echo $company['name']; ?>
 					</a>
-					<?php if (isset($company['url'])): ?>
-					<a class="btn btn-link" href="<?php $parsed = parse_url($company['url']); if (empty($parsed['scheme'])) { echo 'http://' . ltrim($company['url'], '/'); }else{ echo $company['url']; } ?>" target="_blank">
-					<?php echo str_replace("http://"," ",str_replace("www.", "", $company['url']))?>
-					</a>
-					<?php endif; ?>
 				</h3>
 				<?php if (isset($company['class'])): ?>
 				<span class="label label-info"><?php echo $companies_classes[$company['class']] ?></span>
@@ -72,26 +67,26 @@
         <?php if (isset($company['url'])): ?>
         <div class="col-md-12" style="margin-top:5px;">
         <strong>
-				Website
-			</strong>
-				<p style="margin-bottom:0;"><a class="btn btn-link" style="padding:0;" href="<?php $parsed = parse_url($company['url']); if (empty($parsed['scheme'])) { echo 'http://' . ltrim($company['url'], '/'); }else{ echo $company['url']; } ?>" target="_blank"><i class="fa fa-home"></i>
-				<?php echo str_replace("http://"," ",str_replace("www.", "", $company['url']))?>
-				</a></p>
-                </div>
-				<?php endif; ?>
+			Website
+		</strong>
+		<p style="margin-bottom:0;"><a class="btn btn-link" style="padding:0;" href="<?php $parsed = parse_url($company['url']); if (empty($parsed['scheme'])) { echo 'http://' . ltrim($company['url'], '/'); }else{ echo $company['url']; } ?>" target="_blank"><i class="fa fa-home"></i>
+		<?php echo str_replace("http://"," ",str_replace("www.", "", $company['url']))?>
+		</a></p>
+        </div>
+		<?php endif; ?>
                 
-                <!--SEGMENT IF APPLICABLE-->
-                <?php if (isset($company['class'])): ?>
-                 <div class="col-md-12" style="margin-top:5px;">
-	        	<strong>
-					Segment
-				</strong>
-				<p style="margin-bottom:0;">				
-				<span class="label label-info"><?php echo $companies_classes[$company['class']] ?>
-				</span>
-				</p>
-                </div>
-			<?php endif; ?>
+        <!--SEGMENT IF APPLICABLE-->
+        <?php if (isset($company['class'])): ?>
+         <div class="col-md-12" style="margin-top:5px;">
+    	<strong>
+			Segment
+		</strong>
+		<p style="margin-bottom:0;">				
+		<span class="label label-info"><?php echo $companies_classes[$company['class']] ?>
+		</span>
+		</p>
+        </div>
+		<?php endif; ?>
                 
                 
 		<div class="col-md-12">
