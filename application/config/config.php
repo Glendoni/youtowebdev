c6a1bfb4915b286ca2af25661929dca7b2ce53e0
Antonio <tottimix@gmail.com>
2014-10-14 18:48:28 +0100

    mad mod

--- application/config/config.php
+++ application/config/config.php
@@ -266,7 +266,7 @@ $config['sess_time_to_update']	= 300;
 |
 */
 $config['cookie_prefix']	= "baselist";
-$config['cookie_domain']	= "baselist.herokuapp.com";
+$config['cookie_domain']	= ".herokuapp.com";
 $config['cookie_path']		= "/";
 $config['cookie_secure']	= FALSE;
 
