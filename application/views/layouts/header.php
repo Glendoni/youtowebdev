a8215b3beaddde2c4dffd852573bf3f721a9a13c
hanks83 <nharriman@sonovate.com>
2014-10-28 08:33:00 +0000

    Fixed company name search issue

--- application/views/layouts/header.php
+++ application/views/layouts/header.php
@@ -323,7 +323,7 @@
                                     <div class='form-row'>
                                         <div class="col-md-12 form-group ">
                                             <?php  echo form_label('Agency Name', 'agency_name', array('class'=>'control-label')); ?>
-                                            <input name="agency_name" id="agency_name" type="text" onkeyup="ajaxSearch();" class="col-md-12 form-control" autocomplete="off" value=<?php echo $this->input->post('agency_name');?> >
+                                            <input name="agency_name" id="agency_name" type="text" onkeyup="ajaxSearch();" class="col-md-12 form-control" autocomplete="off" value="<?php echo $this->input->post('agency_name');?>" >
                                             <div id="suggestions">
                                                 <div id="autoSuggestionsList">  
                                                 </div>
