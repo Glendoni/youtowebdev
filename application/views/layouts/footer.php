d7356cc305f1a8da5026b8c97623f8acd92d8927
Antonio <tottimix@gmail.com>
2015-01-20 14:25:34 +0000

    moments.js

--- application/views/layouts/footer.php
+++ application/views/layouts/footer.php
@@ -26,7 +26,8 @@
     <script type="text/javascript"  src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.9/jquery.validate.min.js"></script>
     
     <!-- DATE TIME PICKER -->
-    <script type="text/javascript" src="https://eonasdan.github.io/bootstrap-datetimepicker/scripts/moment.js"></script>
+    <!-- <script type="text/javascript" src="https://eonasdan.github.io/bootstrap-datetimepicker/scripts/moment.js"></script> -->
+    <script type="text/javascript" src="<?php echo asset_url();?>js/moment.js"></script>
 
     <script type="text/javascript" src="<?php echo asset_url();?>js/bootstrap-datetimepicker.js"></script>
 
