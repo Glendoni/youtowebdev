fbf007891d79b1c0176bd36aa46f3e5a0337ec6e
Antonio <tottimix@gmail.com>
2015-03-20 01:49:58 +0000

    first campaigns commit

--- application/views/companies/search_results.php
+++ application/views/companies/search_results.php
@@ -10,34 +10,33 @@
 
 		<div class="col-md-8" style="text-align:right;">
 	      	<?php if(($companies_count > 0)): ?>
-				<?php if($current_campaign_name && $current_campaign_owner_id && $current_campaign_id ): ?>
-				
-				<div class="navbar-text saved-search-text">
-				<span class="assigned-image-holder" >
-		        	<img style="height:30px;width:30px;" src="<?php echo asset_url();?>images/profiles/<?php echo isset($system_users_images[$current_campaign_owner_id])? $system_users_images[$current_campaign_owner_id]:'none.jpg' ;?>" class="img-circle img-responsive"  alt="" />
-		        </span>
-					<span style="font-weight:300; font-size:16px;">Search:</span>
-					<?php echo $current_campaign_name; ?>
-				</div>
+				<?php if($current_campaign_name && $current_campaign_owner_id && $current_campaign_id ): ?>	
+					<div class="navbar-text saved-search-text">
+						<span class="assigned-image-holder" >
+				        	<img style="height:30px;width:30px;" src="<?php echo asset_url();?>images/profiles/<?php echo isset($system_users_images[$current_campaign_owner_id])? $system_users_images[$current_campaign_owner_id]:'none.jpg' ;?>" class="img-circle img-responsive"  alt="" />
+				        </span>
+						<span style="font-weight:300; font-size:16px;">Search:</span>
+						<?php echo $current_campaign_name; ?>
+					</div>
 				<?php if($current_campaign_is_shared == False): ?>
-				<div class="navbar-text saved-search-text">
-					<span style="font-weight:300; font-size:10px;"><i class="fa fa-user"></i> Private Search</span>
-				</div>	
+					<div class="navbar-text saved-search-text">
+						<span style="font-weight:300; font-size:10px;"><i class="fa fa-user"></i> Private Search</span>
+					</div>	
 				<?php else: ?> 
-				<div class="navbar-text saved-search-text">
-					<span style="font-weight:300; font-size:10px;"><i class="fa fa-users"></i> Public Search</span>
-				</div>	
+					<div class="navbar-text saved-search-text">
+						<span style="font-weight:300; font-size:10px;"><i class="fa fa-users"></i> Public Search</span>
+					</div>	
 				<?php endif;?>  
 				<?php if($current_campaign_editable): ?>
-                <?php echo form_open(site_url().'campaigns/edit', 'name="edit_campaign" role="form"'); echo form_hidden('campaign_id', $current_campaign_id); ?>
-                <?php if($current_campaign_is_shared == False): ?>
-					<button type="submit" class="btn btn-warning btn-sm btn-search-edit" name="make_public" ><i class="fa fa-users"></i> Make Public</button>
+	                <?php echo form_open(site_url().'campaigns/edit', 'name="edit_campaign" role="form"'); echo form_hidden('campaign_id', $current_campaign_id); ?>
+	                <?php if($current_campaign_is_shared == False): ?>
+						<button type="submit" class="btn btn-warning btn-sm btn-search-edit" name="make_public" ><i class="fa fa-users"></i> Make Public</button>
 					<?php else: ?>
-					<button type="submit" class="btn btn-warning btn-success btn-sm btn-search-edit" name="make_private" ><i class="fa fa-user"></i> Make Private</button>
+						<button type="submit" class="btn btn-warning btn-success btn-sm btn-search-edit" name="make_private" ><i class="fa fa-user"></i> Make Private</button>
 					<?php endif; ?> 
-						<button type="submit" class="btn btn-danger btn-sm btn-search-edit" name="delete" ><i class="fa fa-trash-o fa-lg"></i> </button>
+					<button type="submit" class="btn btn-danger btn-sm btn-search-edit" name="delete" ><i class="fa fa-trash-o fa-lg"></i> </button>
 					<?php echo form_close(); ?>
-				  <?php endif; ?>
+			  	<?php endif; ?>
 			</div>	
 			<?php else: ?>
 			<?php echo form_open(site_url().'campaigns/create', 'name="create_campaign" class="create_campaign navbar-form navbar-left" role="form"'); ?>
@@ -52,7 +51,9 @@
 						<input type="radio" name="public" id="sharedtrue"><i class="fa fa-user"></i> Public
 					</label>
 				</div>
-			    <button type="submit" class="btn btn-primary">Save Search</button>
+			    <button type="submit" name="save_search" class="btn btn-primary">Save Search</button>
+			    or
+			    <button type="submit" name="save_campaign" class="btn btn-warning">Save Campaign</button>
 			  <?php echo form_close(); ?>
 			<?php endif; ?>
 		<?php endif; ?>
