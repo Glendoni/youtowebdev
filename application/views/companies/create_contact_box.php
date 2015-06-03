43892c9461fa45bebaf3eb8449c7251a36b4f3b4
hanks83 <nharriman@sonovate.com>
2015-06-03 18:03:06 +0100

    Add linkedin to contacts and small autocomplete change

--- application/views/companies/create_contact_box.php
+++ application/views/companies/create_contact_box.php
@@ -8,32 +8,49 @@
             <?php $hidden = array('company_id' => $company['id'] , 'user_id' => $current_user['id'],'create_contact'=>'1', 'page_number' => (isset($current_page_number))? $current_page_number:'');
 				echo form_open(site_url().'contacts/create_contact',array('onSubmit'=>'return validateContactForm();','name' => 'create_contact', 'class'=>'create_contact','role'=>"form" ),$hidden); ?>
             <div class="modal-body">
+                <div class="row">
                 <div class="alert alert-danger" id="error_box" style="display:none;" role="alert"></div>
+                <div class="col-md-6">
                 <div class="form-group">
                     <label for="name" class="control-label">First Name*</label>                            
                     <input type="text" name="first_name" value="<?php echo isset($contact->first_name)?$contact->first_name:''; ?>" maxlength="50" class="form-control">
                 </div>
+                </div>
+                <div class="col-md-6">
                 <div class="form-group">
                     <label for="name" class="control-label">Last Name*</label>                            
                     <input type="text" name="last_name" value="<?php echo isset($contact->last_name)?$contact->last_name:''; ?>" maxlength="50" class="form-control">
                 </div>
+                </div>
+                <div class="col-md-6">
                 <div class=" form-group ">
                     <label for="role" class="control-label">Role *</label>                            
                     <input type="text" name="role" value="" id="role" maxlength="50" class="form-control">
                 </div>
+                </div>
+                <div class="col-md-6">
                 <div class=" form-group ">
                     <label for="email" class="control-label">Email</label>                            
                     <input type="email" name="email" value="" id="email" maxlength="50" class="form-control">
                 </div>
-                
+                </div>
+                <div class="col-md-6">
                 <div class=" form-group ">
                     <label for="phone" class="control-label">Phone </label>                            
                     <input type="phone" name="phone" value="" id="phone" maxlength="50" class="form-control">
                 </div>
+                </div>
+                <div class="col-md-6">
+                <div class=" form-group ">
+                    <label for="linkedin_id" class="control-label">LinkedIn ID </label>                            
+                    <input type="linkedin_id" name="linkedin_id" value="" id="linkedin_id" maxlength="50" class="form-control">
+                </div>
+                </div>
+            </div>
             </div>
             <div class="modal-footer">
             	<button type="submit" class="btn btn-sm btn-primary btn-block ladda-button submit_btn" edit-btn="editbtn<?php echo $company['id']; ?>" loading-display="loading-display-<?php echo $company['id']; ?>" data-style="expand-right" data-size="1">
-		        	<span class="ladda-label"> Save changes </span>
+		        	<span class="ladda-label"> Create Contact </span>
 		    	</button>                
                 
             </div>
