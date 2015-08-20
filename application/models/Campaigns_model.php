4c8b0fb3b3c66c0c4bac2f073bdda5fa5313431d
hanks83 <nharriman@sonovate.com>
2015-08-20 12:51:47 +0100

    baselist 2.3

--- application/models/Campaigns_model.php
+++ application/models/Campaigns_model.php
@@ -474,13 +474,13 @@ C.name \"campaign name\" ,
 C.description description ,
 count(distinct T.company_id) campaign_total,
 round (100 * count(distinct (CASE when A.created_at > C.created_at then A.company_id else null END ))::numeric  / count(distinct CO.id)::numeric) \"%\",
-CASE when count(distinct CASE when CO.pipeline ilike 'Prospect' then CO.id END) = 0 then null 
+CASE when count(distinct CASE when CO.pipeline ilike 'Prospect' then CO.id END) = 0 then 0 
 else count(distinct CASE when CO.pipeline ilike 'Prospect' then CO.id END) END campaign_prospects,
-CASE when count(distinct CASE when CO.pipeline = 'Intent' then CO.id END) = 0 then null 
-else count(distinct CASE when CO.pipeline = 'Intent' then CO.id END) END campaign_intent,
-CASE when count(distinct CASE when CO.pipeline = 'Proposal' then CO.id END) = 0 then null
-else count(distinct CASE when CO.pipeline = 'Proposal' then CO.id END)  END campaign_proposals,
-CASE when count(distinct CASE when CO.pipeline = 'Customer' and CO.customer_from > C.created_at then CO.id END) = 0 then null
+CASE when count(distinct CASE when CO.pipeline ilike 'Intent' then CO.id END) = 0 then 0 
+else count(distinct CASE when CO.pipeline ilike 'Intent' then CO.id END) END campaign_intent,
+CASE when count(distinct CASE when CO.pipeline ilike 'Proposal' then CO.id END) = 0 then 0
+else count(distinct CASE when CO.pipeline ilike 'Proposal' then CO.id END)  END campaign_proposals,
+CASE when count(distinct CASE when CO.pipeline ilike 'Customer' and CO.customer_from > C.created_at then CO.id END) = 0 then 0
 else count(distinct 
 CASE when CO.pipeline = 'Customer' and CO.customer_from > C.created_at then CO.id END) END campaign_customers
 FROM   CAMPAIGNS C
