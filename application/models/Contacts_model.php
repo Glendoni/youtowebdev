53abda084eaccc78197a2c6990f1dbe4cee324d8
hanks83 <nharriman@sonovate.com>
2015-08-31 17:08:03 +0100

    baselist 2.4 final changes

--- application/models/Contacts_model.php
+++ application/models/Contacts_model.php
@@ -1,30 +1,37 @@
 <?php
 class Contacts_model extends CI_Model {
+
 	function get_all()
 	{
 		$query = $this->db->get('contacts');	
 		return $query->result();
 	}
+
 	function get_contact_by_id($id)
 	{
 		$query = $this->db->get_where('contacts',array('id'=>$id));	
 		return $query->row();
 	}
+
 	function get_contact_by_name($first_name)
 	{
 		$query = $this->db->get_where('contacts',array('first_name'=>$first_name));	
 		return $query->result();
 	}
+
 	function get_contact_by_email($email)
 	{
 		$query = $this->db->get_where('contacts',array('email'=>$email));	
 		return $query->result();
 	}
+
 	function get_contacts($company_id)
 	{
 		$query = $this->db->get_where('contacts',array('company_id'=>$company_id));	
 		return $query->result();
 	}
+
+
 	function create_contact($first_name,$last_name,$email,$role,$company_id,$created_by,$phone=NULL,$linkedin_id)
 	{
         $contact->first_name = $first_name; // please read the below note
@@ -48,6 +55,7 @@ class Contacts_model extends CI_Model {
         $rows = $this->db->affected_rows();
 	    return $rows;
     }
+
     function update($post){
     	$contact->first_name   = $post['first_name']; // please read the below note
     	$contact->last_name = $post['last_name'];
