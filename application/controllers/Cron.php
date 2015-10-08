bbd16d0268a08bd0bbbf38419c921ef12af32673
hanks83 <nharriman@sonovate.com>
2015-10-08 11:34:08 +0100

    baselist 2.10 marketing updates running on heroku

---
+++ application/controllers/Cron.php
@@ -0,0 +1,20 @@
+<?php
+class Cron extends CI_Controller {
+ 
+    function __construct()
+    {
+        parent::__construct();
+ 
+        // this controller can only be called from the command line
+        if (!$this->input->is_cli_request()) show_error('Direct access is not allowed');
+                $this->load->helper('date');
+
+            $this->load->model('Cron_model');
+
+    }
+ 
+    function cron()
+    {
+        $this->Cron_model->hello();
+    }
+}
