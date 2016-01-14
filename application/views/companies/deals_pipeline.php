Glendoni/youtowebdev

commit c43d721f5919d12518b99bf263a79273b756015d

baselist 2.18 - small dashboard change

application/views/companies/deals_pipeline.php
@@ -180,14 +180,25 @@ success: function (data) {
     </script> 
 
 
-<div class="row ">
+<div class="row">
+          <div class="col-sm-9 col-sm-offset-3 " style="margin-bottom:20px;">
+              <!-- Nav tabs -->
+              <ul class="nav nav-tabs dashboard" role="tablist">
+                <li><button role="tab" class="button btn btn-primary btn-sm deals_pipeline" style="margin-right:10px;" onclick="window.location ='/dashboard'">Dashboard</button></li>
+                <li  class="active"><button role="tab" class="button btn btn-primary btn-sm deals_pipeline" style="margin-right:10px;" onclick="window.location ='/companies/pipeline'">Upcoming Deals</button></li>
+              </ul>
+
+          </div>
+          <div class="col-sm-9 col-sm-push-3">
+          </div>
+        </div>
+
 
 <div  class="mainboxs " style="overflow:hidden;">
 <div class="row ">
 
 <div class="col-md-2">
-<div  class="column delete" placement="delete" aria-hidden="true"
-<span class="glyphicon glyphicon-trash"></span>
+<div  class="column delete" placement="delete" aria-hidden="true">
 </div>
 </div>
 <div class="col-md-8"><h2 class="company-header">Upcoming Deals</h2></div>
@@ -199,30 +210,16 @@ success: function (data) {
 </div>
 </div>
 <div class="col-md-12">
-
 <div id="pipe">
-
 <div id="pipeline_content">
-            
-           
- </div>
-        </div>
-    </div>
-    </div><!--END ROW-->
-
-    
-    
+                     
 </div>
 </div>
+</div>
+</div><!--END ROW-->    
+</div>
 
-    <script src="//code.jquery.com/jquery-1.10.2.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-  <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
-
- <script type="text/javascript"  src="<?php echo asset_url();?>js/deals-pipeline.js"></script> 
-  <script>
- 
-
-        
-        
-    </script>
+<!--<script src="//code.jquery.com/jquery-1.10.2.js"></script>-->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+<script type="text/javascript"  src="<?php echo asset_url();?>js/deals-pipeline.js"></script> 

commit 298935902a30fea104650a9cde31a443c8d89401

Baslist.2.19
Code + To Do

Fixed error introduced in commit c43d721 which caused 500 error when updating database entry on drop.

issue: the class active added to nav upcoming deals was already binned to another drop event listener

Remedy: removed class active in nav li   application/views/companies/deals_pipeline.php

application/views/companies/deals_pipeline.php
@@ -185,7 +185,7 @@ success: function (data) {
               <!-- Nav tabs -->
               <ul class="nav nav-tabs dashboard" role="tablist">
                 <li><button role="tab" class="button btn btn-primary btn-sm deals_pipeline" style="margin-right:10px;" onclick="window.location ='/dashboard'">Dashboard</button></li>
-                <li  class="active"><button role="tab" class="button btn btn-primary btn-sm deals_pipeline" style="margin-right:10px;" onclick="window.location ='/companies/pipeline'">Upcoming Deals</button></li>
+                <li  class=""><button role="tab" class="button btn btn-primary btn-sm deals_pipeline" style="margin-right:10px;" onclick="window.location ='/companies/pipeline'">Upcoming Deals</button></li>
               </ul>
 
           </div>
@@ -201,7 +201,7 @@ success: function (data) {
 <div  class="column delete" placement="delete" aria-hidden="true">
 </div>
 </div>
-<div class="col-md-8"><h2 class="company-header">Upcoming Deals</h2></div>
+<div class="col-md-8"><h2 class="company-header">Deals Forcast</h2></div>
 <div class="col-md-2"></div>
 </div>
 <div class="row" style="margin-top:20px;">

assets/js/deals-pipeline.js
@@ -30,13 +30,13 @@
             if(dd == 01){
 //console.log(mon[(mm-1)]); 
                 
-                $(' <div class="col-md-3 reduced-padding"><div class="colum-header"><h2>'+mon[(mm-1)]+' <span class="header-bold pipeline_will">Will</span></h2></div></div>').appendTo('#pipeline_labels'); 
-$(' <div class="col-md-3 reduced-padding"><div class="colum-header"><h2>'+mon[(mm-1)]+' <span class="header-bold pipeline_should">Should</span></h2></div></div>').appendTo('#pipeline_labels'); 
+                $(' <div class="col-md-3 reduced-padding"><div class="colum-header"><h2>'+mon[(mm-1)]+' <span class="header-bold pipeline_will">Will </span> <span class="pcountwc'+date+' pipeListCounter"></span></h2> </div></div>').appendTo('#pipeline_labels'); 
+$(' <div class="col-md-3 reduced-padding"><div class="colum-header"><h2>'+mon[(mm-1)]+' <span class="header-bold pipeline_should">Should</span> <span class="pcountsc'+date+' pipeListCounter"></span></div> </h2></div>').appendTo('#pipeline_labels'); 
 
 //$(' <div class="column col-md-2" placement="sc'+date+'"><h2>'+mon[(mm-1)]+'<br/>SHOULD</h2></div>').appendTo('#pipeline_content'); 
 //$(' <div class="column col-md-2" placement="wc'+date+'"><h2>'+mon[(mm-1)]+'<br/>WILL</h2></div>').appendTo('#pipeline_content');
-                $(' <div class="col-md-3 reduced-padding"><div class="column"  placement="wc'+date+'"></div></div>').appendTo('#pipeline_content');
-$(' <div class="col-md-3 reduced-padding"><div class="column"  placement="sc'+date+'"></div></div>').appendTo('#pipeline_content'); 
+                $(' <div class="col-md-3 reduced-padding"><div class="column wc'+date+'"  placement="wc'+date+'"></div></div>').appendTo('#pipeline_content');
+$(' <div class="col-md-3 reduced-padding"><div class="column sc'+date+'"  placement="sc'+date+'"></div></div>').appendTo('#pipeline_content'); 
  
 }
 start = new Date(start.setDate(start.getDate() + 1)); //date increase by 1
@@ -72,7 +72,7 @@ start = new Date(start.setDate(start.getDate() + 1)); //date increase by 1
         },
              update: function( event, ui ) {
                  
-  
+                addListQuantity();
             
              }
     });
@@ -137,6 +137,8 @@ start = new Date(start.setDate(start.getDate() + 1)); //date increase by 1
          '</div> ').appendTo("[placement="+placement_holder_prefix+data[key]['efffrom']+"]" );
    
                                     jsonBinder(); //binds listeners to JSON  generated elements retruned back from the server
+                                
+                                addListQuantity();
                         }
                     }
             
@@ -145,18 +147,36 @@ start = new Date(start.setDate(start.getDate() + 1)); //date increase by 1
     
     }
     
-        function  jsonBinder()
-        { //binds listeners to JSON  generated elements retruned back from the server
 
-            $('.portlet').click(function(){
-                $(this).addClass('active');
-            })
 
-            $('.portlet').mouseup(function(){
-                $('.portlet').removeClass('active');
-                $(this).addClass('active');
-                var placement = $(this).parent().attr('placement');
-                //console.log('What was the original placement  '+ placement);
-            })
-        }
+
+
+    function  jsonBinder()
+    { //binds listeners to JSON  generated elements retruned back from the server
+
+        $('.portlet').click(function(){
+            $(this).addClass('active');
+        })
+
+        $('.portlet').mouseup(function(){
+            $('.portlet').removeClass('active');
+            $(this).addClass('active');
+            var placement = $(this).parent().attr('placement');
+            //console.log('What was the original placement  '+ placement);
+        })
+    }
+
+
+    function addListQuantity(){
+
+        var clid 
+        var clidListQty
+        $.each($('.column'), function(index) { 
+            clid =$(this).attr('placement');
+            clidListQty  = $('.'+clid + ' .portlet').length;
+            $('.pcount'+clid).text(clidListQty)
+
+        });
+
+    }
     