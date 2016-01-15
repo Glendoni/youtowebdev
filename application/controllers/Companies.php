093af2bb692cc29bd3964d875511533510bd1fcc
Glendon <gsmall@sonovate.com>
2016-01-15 09:44:13 +0000

    Baslist.2.19
    Added Linkedin image link to company search results

--- application/controllers/Companies.php
+++ application/controllers/Companies.php
@@ -436,8 +436,14 @@ class Companies extends MY_Controller {
         	if(!empty($row->user)) { 
         		$user_icon = explode(",", $row->image);
         		$assigned_label = "| <span class='label label-primary' style='background-color:".$user_icon[1]."; color:".$user_icon[2].";'>".$row->user."</span>";};
-        	 
-		$response= $response."<a href='". base_url() . "companies/company?id=" . $row->id . "'><li class='autocomplete-item autocomplete-company'><strong>" . str_replace($words, ' ',$row->name). '<a href="https://www.linkedin.com/'.$row->linkedin_id."> <i class='fa fa-caret-square-o-down'></i> </a></strong><br><small>".$row->pipeline." ".$assigned_label."</small></li></a>";
+  $linkedin_id  =   $row->linkedin_id ? "<a target=\"_blank\"  href=\"https://www.linkedin.com/in/".$row->linkedin_id."\" ><img class=\"search_linkedin_img\" src=\"https://static.licdn.com/scds/common/u/images/logos/linkedin/logo_in_nav_44x36.png\"></a>" : "";  
+        
+        
+		$response= $response."<a href='". base_url() . "companies/company?id=" . $row->id . "'><li class='autocomplete-item autocomplete-company'><strong>" . str_replace($words, ' ',$row->name). 
+            " 
+            </strong></a>" .$linkedin_id."<br><small>".$row->pipeline." ".$assigned_label."</small>
+            
+            </li>";
          //$callCH = false;
         endforeach;
         $response= $response."</ul></div>";
