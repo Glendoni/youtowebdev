fb93ca0d5a95e971e9945ff9e7f8489b432e711b
hanks83 <nharriman@sonovate.com>
2016-02-09 21:55:11 +0000

    baselist 3.1 action names

--- application/views/layouts/header.php
+++ application/views/layouts/header.php
@@ -74,6 +74,12 @@
 <!--<div class="navbar-header">-->
 <div class="col-sm-3 col-md-3">
 <a class="navbar-brand" href="/" ><img src="<?php echo asset_url();?>images/baselist-logotype-black-transparent-bg.png" style="width:120px;">
+    <?php if (ENVIRONMENT  =='staging'){?>
+Staging    <?php
+} else {?>
+<?php
+}; ?>
+
 <?php if (isset($current_user)): ?>
 <div style="font-size: 10px;font-weight: 300;text-align: right;float: right;line-height: 40px;padding-left: 5px;">Version 3.1</div>
 <?php endif; ?>
