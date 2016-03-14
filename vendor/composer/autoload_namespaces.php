bb3ceb1b9db0cfeff4c7843cd71fda057fff98d4
Glendon <gsmall@sonovate.com>
2016-03-14 15:58:20 +0000

    Initial Commit

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -6,8 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'Psr\\Log\\' => array($vendorDir . '/psr/log'),
-    'Pimple' => array($vendorDir . '/pimple/pimple/lib'),
-    'Herrera\\Pdo' => array($vendorDir . '/herrera-io/pdo-log/src/lib', $vendorDir . '/herrera-io/silex-pdo/src/lib'),
     'Composer\\Installer' => array($vendorDir . '/compwright/codeigniter-installers/src'),
 );
