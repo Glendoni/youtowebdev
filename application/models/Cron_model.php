283bc39a60f504a77c622b002949091c2e7f2409
Glendon <gsmall@sonovate.com>
2016-03-16 14:26:31 +0000

    Merge branch 'staging'
    # Conflicts:
    #	application/models/Cron_model.php

--- application/models/Cron_model.php
+++ application/models/Cron_model.php
@@ -786,7 +786,39 @@ public function get_campaign_name($campaign)
     
 
 // NEW AUTOPILOT END
+    
+    
+    
+    
      public function getCompanyHouseDetails($url = 0) 
+	{$curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 264,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_HTTPHEADER => array(
+                "autopilotapikey: ed278f3d19a5453fb807125aa945a81a",
+                "cache-control: no-cache",
+                "content-type: application/json",
+                "postman-token: 57861545-5cf1-95d1-c82d-035b6fc32a51"
+            ),
+        ));
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+        curl_close($curl);
+        if ($err) {
+            echo "cURL Error #:" . $err;
+        } else {
+            $result  = json_decode($response, true);
+        return  $result; 
+        }
+    }
+         public function _getCompanyHouseDetails($url = 0) 
 	{
  
         $server_output = array();
