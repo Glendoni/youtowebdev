1a51894bedcdcca9b4d0e05f559ec1505b33074d
hanks83 <nharriman@sonovate.com>
2016-03-29 18:00:14 +0100

    linkedin update

--- application/views/companies/action_box_addresses.php
+++ application/views/companies/action_box_addresses.php
@@ -6,7 +6,7 @@
         <div class="modal-content">
         	<div class="modal-header">
 	        	<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-	        	<h4 class="modal-title">Edit Address</h4>
+	        	<h4 class="modal-title">Edit Location</h4>
 	      	</div>
 	      	<?php $hidden = array('address_id' => $address->addressid , 'user_id' => $current_user['id'],'update_address'=>'1','company_id'=>$address->company_id );
 				 echo form_open(site_url().'companies/update_address', 'name="update_address" class="form" role="form"',$hidden); ?>
@@ -43,7 +43,7 @@
 				 </div>
 	      	</div>
 	      	<div class="modal-footer">
-	        	<button type="submit" class="btn btn-sm btn-warning btn-block">Save</button>
+	        	<button type="submit" class="btn btn-sm btn-warning btn-block">Add Location</button>
 	      	</div>
             <div class="modal-footer">
             <div><small>
