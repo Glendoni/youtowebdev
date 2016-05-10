661dcfacc99835b723f195c6e8328fa030c509e9
hanks83 <nharriman@sonovate.com>
2016-05-10 06:26:15 +0100

    3.8 linkedin search icon

--- application/views/companies/list.php
+++ application/views/companies/list.php
@@ -173,7 +173,7 @@ if ($your_date < $now){; ?>
 			<?php if (isset($company['linkedin_id'])): ?>
 			<a class="btn  btn-info btn-sm btn-block linkedin" href="https://www.linkedin.com/company/<?php echo $company['linkedin_id'] ?>"  target="_blank">LinkedIn</a>
 			  <?php else: ?>
-              <a class="btn  btn-primary btn-sm btn-block" href="https://www.linkedin.com/vsearch/f?type=all&keywords=<?php echo  urlencode($company['name']) ?>"  target="_blank">Search LinkedIn</a>
+              <a class="btn  btn-primary btn-sm btn-block" href="https://www.linkedin.com/vsearch/f?type=all&keywords=<?php echo  urlencode($company['name']) ?>"  target="_blank"><i class="fa fa-search" aria-hidden="true"></i> Search LinkedIn</a>
             <?php endif; ?>
             
             
