fcdee789e9ca118fea5a29b93f093f004de06787
Glendon <gsmall@sonovate.com>
2016-06-20 15:43:49 +0100

    Baselist 3.10
    Update to sticky actions menu.
    
    Issue: When few actions are displayed the menu would flicker a number of times then stop.
    
    Remedy: Reduce scroll to and changed padding attribute value

--- application/views/companies/company.php
+++ application/views/companies/company.php
@@ -896,7 +896,7 @@ if ($your_date < $now){;
 
                 if(posY < 0){
                     $('.sticky').addClass('affix');
-                    $('.affix').css('padding-top', '144px')
+                    $('.affix').css('padding-top', '120px')
                 }else{
                     $('.sticky').removeClass('affix');
                 }
