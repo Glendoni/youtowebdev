10552f48cd680996e57cf5fee9f41fbb570080b8
Glendon <gsmall@sonovate.com>
2016-07-05 16:20:43 +0100

    Baselist 3.11
    On contacts list - contacts that have left should be listed last

--- application/views/companies/edit_box.php
+++ application/views/companies/edit_box.php
@@ -30,8 +30,7 @@
                 </div>
             </div>
         </div>
-            
-            
+               
         <div class="row">
             <div class="col-sm-6 col-md-3" style="display:none;">
                 <div class="row">
