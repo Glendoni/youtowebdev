2b606481941c0da80794eedf24cacee40e4ffd8f
Glendon <gsmall@sonovate.com>
2016-07-13 15:05:57 +0100

    Baselist 3.11
    Gmail tester

--- delete.php
+++ delete.php
@@ -1,6 +1,21 @@
 <?php
 
-// Show all information, defaults to INFO_ALL
-phpinfo();
+ set_time_limit(3000); 
+ /* connect to gmail with your credentials */
+$hostname = "{imap.gmail.com:993/imap/ssl}INBOX";
+    $email = "gsmall@sonovate.com";
+    $password = "LiverpoolFC";
+/* try to connect */
+$inbox = imap_open($hostname,$username,$password) or die('Cannot connect to Gmail: ' . imap_last_error());
 
-?>
+
+
+        
+        
+        
+        
+        
+        //	$this->data['main_content'] = 'companies/editor';
+		//$this->load->view('layouts/default_layout', $this->data);	
+        
+    
