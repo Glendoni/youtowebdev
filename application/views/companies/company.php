86076a67effbc3b98cda9152c3f2555d871ac3d0
Glendon <gsmall@sonovate.com>
2016-08-26 11:34:21 +0100

    Baselist 3.14
    Restored missing recruitment

--- application/views/companies/company.php
+++ application/views/companies/company.php
@@ -309,7 +309,7 @@ if ($your_date < $now){;
 		</a>
 		<?php else: ?>
 
-    <a class="btn  btn-default btn-sm btn-block " href="https://www.google.co.uk/search?q=<?php echo str_replace('&','',$company['name']);  ?>"  target="_blank">Google <i class="fa fa-search" aria-hidden="true"></i></a>
+    <a class="btn  btn-default btn-sm btn-block " href="https://www.google.co.uk/search?q=<?php echo str_replace('&','',$company['name']);  ?> recruitment"  target="_blank">Google <i class="fa fa-search" aria-hidden="true"></i></a>
             <?php endif; ?>
 			
 			<?php if (isset($company['registration'])): ?>
