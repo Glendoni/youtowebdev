a90c796c3d8591b16c0cb436f5b35c25e97dd9d8
Glendon <gsmall@sonovate.com>
2016-10-11 12:50:25 +0100

    Baselist 4.1
    Update to pop up taging functionality to maintain display after events

--- assets/js/fe_tagging.js
+++ assets/js/fe_tagging.js
@@ -86,7 +86,7 @@ function populate(){
                 subcatName = val['sub_cat_name'].replace(/\s/gi, "");
                 subcatName = subcatName.replace('(', '');
                 subcatName = subcatName.replace(')', '');
-                tagcont.push('<div class="col-xs-12 '+subcatName+' fetagsholder'+val['tac_sub_cat_id']+' added-tag-holder"><div class="category-name-holder" >'+val['sub_cat_name']+'</div><ul class="fetags'+val['tac_sub_cat_id']+' sub_ul"></ul></div>'); 
+                tagcont.push('<div class="col-xs-3 '+subcatName+' fetagsholder'+val['tac_sub_cat_id']+' added-tag-holder"><div class="category-name-holder" >'+val['sub_cat_name']+'</div><ul class="fetags'+val['tac_sub_cat_id']+' sub_ul"></ul></div>'); 
                 }          
 
                 vale = val;
@@ -136,7 +136,7 @@ function populate(){
             tagSearch();
                    //scScroll()
 
-            $('.tagContainer .list-group-item').click(function(){
+            $('.tagContainer .list-group-item_').click(function(){
 
                 if($(this).hasClass('folder') &&$(this).hasClass('activeMain')  ){
                     $('.folder').removeClass('activeMain');
@@ -411,7 +411,7 @@ return plainDate;
                         count++;
                         if($('#filter').val() === ''){
 
-                        $('.sub_group').hide();
+                       // $('.sub_group').hide();
                         $('.parent').show();
 
                         $(this).val(), count = 0;
