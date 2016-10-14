27874907792b43f1c1b29481e2c4fe7550b24a95
Glendon <gsmall@sonovate.com>
2016-10-14 11:08:06 +0100

    Baselist 4.0
    Class allocation cron job and run time code patched to live.

--- application/models/Cron_model.php
+++ application/models/Cron_model.php
@@ -1142,7 +1142,10 @@ AND C.customer_from between M.eff_from and (CASE when M.eff_to is not null then 
 
 where customer_from is not null
  and M.inv_fin_related <> \'N\'
-and M.inv_fin_related in (\'Y\',\'P\')
+
+ and M.inv_fin_related not in (\'Y\',\'P\')
+
+or M.stage <> \'Satisfied\'  or M.provider_id <> 28935
  
   ) T2
 ON C.id = T2.id
@@ -1160,8 +1163,14 @@ AND T.category_id = 13
 ON C.id = T3.company_id
 
 where C.customer_from is not null
-and CASE when T2.id is not null or T3.company_id is not null then \'Using Finance\' else \'FF\' END <> class
+and CASE when T2.id is not null or T3.company_id is not null  then \'Using Finance\' else \'FF\' END <> class
 order by customer_from desc';
+        
+        
+        
+        
+        $sql_ = "select company_id FROM MORTGAGES M 
+                        WHERE M.inv_fin_related  in ('Y','P') and M.stage  in ('Outstanding')";
         
         
         $query = $this->db->query($sql) ;     
@@ -1184,9 +1193,78 @@ order by customer_from desc';
                  echo '</table>';
             
             
-         
+          $this->checkClassDateUpdater();
+            $this->checkClassDateUpdaterSonovate();
              }
     }
+    
+    
+    
+    
+function  checkClassDateUpdater(){
+        
+        $sql= 'select  C.id 
+
+from companies C
+
+LEFT JOIN MORTGAGES M
+ON C.id = M.company_id
+
+
+where C.customer_from between M.eff_from and (CASE when M.eff_to is not null then M.eff_to else \'2100-01-01\'::date END) and M.provider_id <> 28935 or M.stage <> \'Satisfied\'  and M.provider_id <> 28935 '; 
+        
+        
+          $query = $this->db->query($sql) ; 
+        
+               echo '<table width="400">';
+           foreach($query->result() as $row)
+                    {         
+                            echo '<tr><td align="left" class="glen">'.$row->id.'</td><td align="left" class="glen">'.$row->id.'</td>';
+                                //$this->cronpipelineUpdater($row->id,'Using Finance');  } 
+                      $this->cronpipelineUpdaternew($row->id,'Using Finance');
+                            
+                    }
+        
+        
+          echo '</table>';
+        
+        
+    }
+    
+    
+    function  checkClassDateUpdaterSonovate(){
+        
+        $sql= 'select  C.id 
+
+from companies C
+
+LEFT JOIN MORTGAGES M
+ON C.id = M.company_id
+where M.provider_id = 2893 '; 
+        
+        
+          $query = $this->db->query($sql) ; 
+        
+               echo '<table width="400">';
+           foreach($query->result() as $row)
+                    {         
+                            echo '<tr><td align="left" class="glen">'.$row->id.'</td><td align="left" class="glen"> sonovate '.$row->id.'</td>';
+                                //$this->cronpipelineUpdater($row->id,'Using Finance');  } 
+                      //$this->cronpipelineUpdaternew($row->id,'FF');
+                            
+                    }
+        
+        
+          echo '</table>';
+        
+        
+    }
+    
+    
+    
+    
+    
+    
     
     
         function cronpipelineUpdaternew($id,$pipeline){ 
