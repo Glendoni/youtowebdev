202a573f142e038dfd8d8eefe568ece2d088a8d5
Glendon <gsmall@sonovate.com>
2016-10-31 14:27:37 +0000

    Baselist 4.2
    SSL tester :  querychecker

--- application/controllers/Companies.php
+++ application/controllers/Companies.php
@@ -1127,12 +1127,18 @@ echo floor($datediff / (60 * 60 * 24));
     echo    $a =  $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
       
        echo '<br>';
-      if (strpos($a, 'http://') !== false) {
-    echo 'site url is true';
-}else{
-          
-       echo 'site url is ssl ';   
-      }
+  $mystring = $_SERVER['HTTP_HOST'];
+$findme   = 'https:';
+$pos = strpos($mystring, $findme);
+
+// Note our use of ===.  Simply == would not work as expected
+// because the position of 'a' was the 0th (first) character.
+if ($pos === false) {
+    echo "The string '$findme' was not found in the string '$mystring'";
+} else {
+    echo "The string '$findme' was found in the string '$mystring'";
+    echo " and exists at position $pos";
+}
       
       
       
@@ -1141,6 +1147,9 @@ echo floor($datediff / (60 * 60 * 24));
         $url = "https:/" . $_SERVER['REQUEST_URI'];
     }
     echo  $url;
+      
+      
+      echo (strpos($a, 'http://'));
     
 }
     
