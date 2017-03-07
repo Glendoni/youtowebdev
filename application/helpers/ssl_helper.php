32e6c86fef75fb17f17558ab4cbf812dfbb22ea0
James Flight <jflight@sonovate.com>
2017-03-07 13:02:09 +0000

    ssl-fix - Fixed ssl

--- application/helpers/ssl_helper.php
+++ application/helpers/ssl_helper.php
@@ -1,6 +1,10 @@
 <?php
 
 function getHttpsUrl($herokuForwardedProto, $url, $env) {
+    if (! isset($herokuForwardedProto)) {
+        throw new Exception('No http on local environment.');
+    }
+    
     if (isset($herokuForwardedProto) && $herokuForwardedProto == "https") {
         throw new Exception('Https already enabled');
     }
