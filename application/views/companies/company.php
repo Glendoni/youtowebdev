95de056206b7df2907c736c89f72a1aeb0cc7225
Glendon <gsmall@sonovate.com>
2017-04-27 13:20:33 +0100

    Sprint 4 Lazer unicorns - Sprint 1 - BUG - TEST Baselist - ensure proposal is created prior to deal being done.

--- application/views/companies/company.php
+++ application/views/companies/company.php
@@ -1495,7 +1495,11 @@ endif;
                         <div class="col-sm-4 col-md-4">
                             <div class="form-group ">
                                 <label>New Action</label>
+                           <?php
+                             echo   $proposal_set_check[0]['count'] ? 'There is a proposal set'.$proposal_set_check[0]['count'] : 'There is no proposal set'. $proposal_set_check[0]['count'] ;
+                                 unset($action_types_done[17]);
                                 
+                                ?>
                                 <select id="action_type_completed" name="action_type_completed" class="form-control" >
                                     <option value="">--- Select an Action ---</option>
                                 <?php if(!$proposal_set_check[0]['count']) unset($action_types_done[17]); ?>
