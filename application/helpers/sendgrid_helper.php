90bf2cea9e3c662db9819379fe27837ec403897b
Glendon <gsmall@sonovate.com>
2017-05-22 13:34:37 +0100

    Sprint 3
    Sendesk integration - Blueprint.
    
    vendor/composer/include_paths.php
    vendor/composer/autoload_static.php
    vendor/composer/autoload_files.php
    application/helpers/sendgrid_helper.php
    application/controllers/Email_templates.php
    composer.lock
    composer.json

---
+++ application/helpers/sendgrid_helper.php
@@ -0,0 +1,25 @@
+<?php require 'vendor/autoload.php';
+
+function send_me($contact_email =  "gsmall@sonovate.com", $subject ="Who you looking at you mook", $email_body = "<p>Finer where </p>", $sender_email = "gsmall@sonovate.com"){
+     
+    
+$from = new SendGrid\Email(null, $sender_email);
+$subject = $subject;
+$to = new SendGrid\Email(null, $contact_email );
+$content = new SendGrid\Content("text/html", $email_body);
+$mail = new SendGrid\Mail($from, $subject, $to, $content);
+    
+$apiKey = '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';
+$sg = new \SendGrid($apiKey);
+
+try {
+    $response = $sg->client->mail()->send()->post($mail);
+} catch (Exception $e) {
+    echo 'Caught exception: ',  $e->getMessage(), "\n";
+}
+
+return $response->statusCode();
+//echo $response->headers();
+//echo $response->body();
+    
+}
