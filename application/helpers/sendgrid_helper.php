8de43e03fbbfc8646674087f82d919c04504ce37
Glendon <gsmall@sonovate.com>
2017-05-23 15:41:27 +0100

    Laser Unicorns - Sprint 2/3 - Send grid integration
    Added environmental variable to read API key.

--- application/helpers/sendgrid_helper.php
+++ application/helpers/sendgrid_helper.php
@@ -1,6 +1,6 @@
 <?php require 'vendor/autoload.php';
 
-function send_me($contact_email =  "gsmall@sonovate.com", $subject ="Who you looking at you mook", $email_body = "<p>Finer where </p>", $sender_email = "gsmall@sonovate.com"){
+function send_me($contact_email =  "gsmall@sonovate.com", $subject ="Who you looking at you mook", $email_body = "<p>Test message</p>", $sender_email = "gsmall@sonovate.com"){
      
     
 $from = new SendGrid\Email(null, $sender_email);
@@ -9,7 +9,14 @@ $to = new SendGrid\Email(null, $contact_email );
 $content = new SendGrid\Content("text/html", $email_body);
 $mail = new SendGrid\Mail($from, $subject, $to, $content);
     
+    if(ENVIRONMENT = 'development'){
 $apiKey = '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';
+    }else{
+        
+      $apiKey = SEND_GRID_API_KEY;  
+        
+    }
+    
 $sg = new \SendGrid($apiKey);
 
 try {
