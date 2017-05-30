3c931c5cd19c9371996dc968d5163f7fcd3ec17b
Glendon <gsmall@sonovate.com>
2017-05-30 16:23:45 +0100

    5428 - Emails for Baselist - ensure dependency on gmail for sending mails cut
    Problem: Unable to send emails without files
    Solved: updated  json construct to evaluate if files are present in send message.

--- application/helpers/sendgrid_helper.php
+++ application/helpers/sendgrid_helper.php
@@ -21,28 +21,32 @@ function send_grid_mailer($contact_email =  "gsmall@sonovate.com", $subject ="Wh
       "type": "text/html",
       "value": "'.str_replace('\'', '&apos;', $email_body).'"
     }
-  ],
-	"attachments": [';
-
-  foreach($attachments as $key => $file_info){
-   $b[] =  '{
-   "content" : "'.base64_encode(file_get_contents($file_info['full_path'])).'",
-    "content_id": "'.$file_info['raw_name'].'_'.date('Y-m-d').'",    
-    "disposition": "inline", 
-    "filename": "'.$file_info['file_name'].'", 
-    "name": "'.$file_info['orig_name'].'",
-    "type": "'.$file_info['file_ext'].'"
-        } ';
-}
+  ],';
+    
+    if(count($attachments) >=1){
         
-$a[] = join($b,",");
-
-$a[] ='],
-  "template_id": "49dd101c-ec16-4636-bbb4-a9f2b067b7a9"
-}';
+        $a[] = '"attachments": [';
+        foreach($attachments as $key => $file_info){
+           $b[] =  '{
+           "content" : "'.base64_encode(file_get_contents($file_info['full_path'])).'",
+            "content_id": "'.$file_info['raw_name'].'_'.date('Y-m-d').'",    
+            "disposition": "inline", 
+            "filename": "'.$file_info['file_name'].'", 
+            "name": "'.$file_info['orig_name'].'",
+            "type": "'.$file_info['file_ext'].'"
+                } ';
+            
+            unlink($file_info['full_path']);
+        }        
+        $a[] = join($b,",");
+        $a[] ='],';
+    }
+    
+    $a[] = '"template_id": "49dd101c-ec16-4636-bbb4-a9f2b067b7a9"
+    }';
   
     
-    $request_body =  join($a);  
+    $request_body =  join($a,"");  
     
     
     $request_body = json_decode($request_body);
